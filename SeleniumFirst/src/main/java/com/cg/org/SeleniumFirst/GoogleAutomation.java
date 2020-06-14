5571dbbe6e20b6b0f82cd8ad4b3c170211d10b50
Rincy-Cheriyan <Rincy3230@gmail.com>
2020-06-14 22:38:19 +0530

    commit from eclipse

---
+++ SeleniumFirst/src/main/java/com/cg/org/SeleniumFirst/GoogleAutomation.java
@@ -0,0 +1,31 @@
+package com.cg.org.SeleniumFirst;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleAutomation {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("First Maven Project");
+		
+		
+	
+		
+		
+		
+
+	}
+
+}
